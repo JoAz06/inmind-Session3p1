73435878d316ff60ab8a162013f519f67c8d650f
Joseph <Josephazzi2006@gmail.com>
2026-02-14 15:06:19 +0200

    Scaffold

---
+++ inmind-Session3p1/Models/Course.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace inmind_Session3p1.Models;
+
+public partial class Course
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public int TeacherId { get; set; }
+
+    public virtual Teacher Teacher { get; set; } = null!;
+}
